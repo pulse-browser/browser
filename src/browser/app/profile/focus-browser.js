79827742cc9e70c0115dfa6eccf7112753306602
trickypr <trickypr@icloud.com>
2021-11-30 15:53:22 +1100

    🐛 Use extensions.installedDistroAddon to prevent permission popups

--- src/browser/app/profile/focus-browser.js
+++ src/browser/app/profile/focus-browser.js
@@ -4,3 +4,8 @@ pref('browser.aboutwelcome.enabled', false);
 
 pref('focus.welcome.enabled', true);
 pref('focus.welcome.seen', false);
+
+// This preference tells the browser that our addons are preinstalled and should
+// be provided with permissions without asking
+// TODO: Documentation in melon for preference
+pref('extensions.installedDistroAddon.newtab@browser.fushra.com', true);
