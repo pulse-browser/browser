bd190fbbda447571ffa1773bd121acbbe4653a39
trickypr <trickypr@icloud.com>
2022-01-27 20:08:14 +1100

    🚩 Feature flag for showing and hiding sidebar

--- src/browser/app/profile/pulse-browser.js
+++ src/browser/app/profile/pulse-browser.js
@@ -1,6 +1,7 @@
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
+//
 // Note: You must have semicolons at the end of each line in user setting files
 
 // Enable downloading drm
@@ -12,6 +13,9 @@ pref('browser.aboutwelcome.enabled', false);
 pref('pulse.welcome.enabled', true);
 pref('pulse.welcome.seen', false);
 
+// Sidebar pref
+pref('pulse.sidebar.enabled', true);
+
 // This preference tells the browser that our addons are preinstalled and should
 // be provided with permissions without asking
 // TODO: Documentation in melon for preference
@@ -22,6 +26,6 @@ pref('svg.context-properties.content.enabled', true);
 
 // Allow the user to install unsigned addons from sources like our custom addon
 // store
-pref('xpinstall.signatures.required', false)
+pref('xpinstall.signatures.required', false);
 
 #include better-fox.js
