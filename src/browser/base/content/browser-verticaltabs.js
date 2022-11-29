08f513258a3ae9e5c4de01c274483bd25e14fe4c
trickypr <trickypr@icloud.com>
2022-11-29 20:50:36 +1100

    💄 Better UI for newtab button etc

--- src/browser/base/content/browser-verticaltabs.js
+++ src/browser/base/content/browser-verticaltabs.js
@@ -1,8 +1,9 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+// @ts-check
 
-const VERTICAL_TABS_POSITION = 'browser.tabs.verticalPosition'
+const VERTICAL_TABS_POSITION = 'pulse.tabs.vertical'
 
 var VerticalTabs = {
   /**
@@ -13,27 +14,29 @@ var VerticalTabs = {
   },
 
   /**
-   * @return {HTMLElement}
+   * @return {HTMLElement?}
    */
   get tabsToolbar() {
     return document.getElementById('TabsToolbar')
   },
 
   /**
-   * @return {HTMLElement}
+   * @return {HTMLElement?}
    */
   get titlebarContainer() {
     return document.getElementById('titlebar')
   },
 
   /**
-   * @return {HTMLElement}
+   * @return {HTMLElement?}
    */
   get browserContainer() {
     return document.getElementById('browser')
   },
 
+  /** @type {HTMLElement?} */
   arrowScrollbox: null,
+  /** @type {HTMLElement?} */
   tabBrowserTabs: null,
 
   init() {
@@ -50,31 +53,33 @@ var VerticalTabs = {
   enableVerticalTabs() {
     console.log('enableVerticalTabs')
 
-    this.browserContainer.prepend(this.tabsToolbar)
+    this.browserContainer?.prepend(this.tabsToolbar || '')
 
-    setTimeout(() => {
-      this.arrowScrollbox.setAttribute('orient', 'vertical')
-      this.tabBrowserTabs.setAttribute('orient', 'vertical')
-    }, 50)
+    this.arrowScrollbox?.setAttribute('orient', 'vertical')
+    this.tabBrowserTabs?.setAttribute('orient', 'vertical')
 
-    this.resetTabBrowser()
+    document
+      .getElementById('navigator-toolbox-background')
+      ?.setAttribute('verticaltabs', 'true')
+    document
+      .querySelector('#TabsToolbar .toolbar-items')
+      ?.setAttribute('align', 'start')
   },
 
   disableVerticalTabs() {
     console.log('disableVerticalTabs')
 
-    this.titlebarContainer.prepend(this.tabsToolbar)
+    this.titlebarContainer?.prepend(this.tabsToolbar || '')
 
-    setTimeout(() => {
-      this.arrowScrollbox.setAttribute('orient', 'horizontal')
-      this.tabBrowserTabs.setAttribute('orient', 'horizontal')
-    }, 50)
+    this.arrowScrollbox?.setAttribute('orient', 'horizontal')
+    this.tabBrowserTabs?.setAttribute('orient', 'horizontal')
 
-    this.resetTabBrowser()
-  },
-
-  resetTabBrowser() {
-    delete window.gBrowser._tabs
+    document
+      .getElementById('navigator-toolbox-background')
+      ?.removeAttribute('verticaltabs')
+    document
+      .querySelector('#TabsToolbar .toolbar-items')
+      ?.setAttribute('align', 'end')
   },
 
   /**
