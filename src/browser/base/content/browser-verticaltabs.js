cd8aac357220d985f279e42e73722740671009ea
TrickyPR <23250792+trickypr@users.noreply.github.com>
2023-07-13 19:41:14 +1000

    🧑‍💻 Include gecko types for autocomplete

--- src/browser/base/content/browser-verticaltabs.js
+++ src/browser/base/content/browser-verticaltabs.js
@@ -2,6 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 // @ts-check
+///<reference types="gecko-types" />
 
 const VERTICAL_TABS_POSITION = 'pulse.tabs.vertical'
 const VERTICAL_TABS_COLLAPSE = 'pulse.tabs.vertical.collapse'
